38e95f7855c4df90144d46e7fa9cf311cade5d84
Your Name <you@example.com>
2025-09-26 03:12:33 +0600

    first commit

---
+++ Primitive.js
@@ -0,0 +1,6 @@
+var username = 'sneara';
+var age = 22;
+var isLogged = true;
+console.log("Name:", username);
+console.log("Age:", age);
+console.log("Is Logged:", isLogged);
---
+++ Primitive.ts
@@ -0,0 +1,6 @@
+let username:string='sneara';
+let age: number=22;
+let isLogged:boolean=true;
+console.log("Name:",username);
+console.log("Age:",age);
+console.log("Is Logged:",isLogged);
