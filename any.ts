a705aa196ced53a83a213a52b0c8ae2c08a2d881
Your Name <you@example.com>
2025-10-08 11:17:23 +0600

    added enum & any file

---
+++ any.ts
@@ -0,0 +1,6 @@
+let randomValue : any=10;
+console.log('Random Value:',randomValue);
+randomValue='snrara';
+console.log('Random Value:',randomValue);
+randomValue=true;
+console.log('Random Value:',randomValue)
--- array.ts
+++ array.ts
@@ -1,4 +1,4 @@
 let number:number[]=[1,2,3,4,5];
 let fruits:string[]=['appale','banana','grapes'];
-console.log('Numbers:', number);
-console.log('fruits',fruits)
+console.log('Numbers:',number);
+console.log('fruits:',fruits)
